639888c82e63e58eecfffb8e7fe10881077ca461
Rafael Dalcolmo <9065913+RDalcolmo@users.noreply.github.com>
2024-11-18 18:25:35 -0500

    Updated to API 11

--- FastJobSwitcher/Service.cs
+++ FastJobSwitcher/Service.cs
@@ -1,15 +1,14 @@
 using Dalamud.IoC;
 using Dalamud.Plugin.Services;
 
-namespace FastJobSwitcher
+namespace FastJobSwitcher;
+
+public class Service
 {
-  public class Service
-  {
 #pragma warning disable CS8618
     [PluginService] public static IDataManager Data { get; private set; }
     [PluginService] public static IChatGui ChatGui { get; private set; }
     [PluginService] public static ICommandManager Commands { get; private set; }
     [PluginService] public static IPluginLog PluginLog { get; private set; }
 #pragma warning restore CS8618
-  }
 }
