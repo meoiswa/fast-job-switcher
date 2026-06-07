6fd2a05169431ae2e16c0254d0443227e08182e2
Meoiswa <meoiswa@gmail.com>
2026-06-07 18:00:07 +0200

    feat: added help tooltip for base commands

--- FastJobSwitcher/FastJobSwitcherUI.cs
+++ FastJobSwitcher/FastJobSwitcherUI.cs
@@ -44,11 +44,68 @@ public class FastJobSwitcherUI : Window, IDisposable
         ImGui.Indent();
         {
             var classJobEnabled = configuration.RegisterClassJobs;
-            if (ImGui.Checkbox("Classes and Jobs##ClassJobs", ref classJobEnabled))
+            ImGui.BeginGroup();
+            ImGui.PushID("ClassJobsRow");
+            ImGui.Checkbox("Classes and Jobs##ClassJobs", ref classJobEnabled);
+            ImGui.SameLine();
+            ImGui.PushStyleVar(ImGuiStyleVar.FrameRounding, 999f);
+            ImGui.PushStyleVar(ImGuiStyleVar.FramePadding, new Vector2(2, 2));
+            if (ImGui.Button("?", new Vector2(22, 22))) { /* No action on click */ }
+            if (ImGui.IsItemHovered())
+            {
+                ImGui.BeginTooltip();
+                ImGui.TextUnformatted("Use each class or job acronym as a command, for example:");
+                ImGui.Spacing();
+                if (ImGui.BeginTable("ClassJobExamples", 2, ImGuiTableFlags.BordersInnerV | ImGuiTableFlags.PadOuterX))
+                {
+                    ImGui.TableSetupColumn("Command");
+                    ImGui.TableSetupColumn("Matches");
+                    ImGui.TableHeadersRow();
+
+                    ImGui.TableNextRow();
+                    ImGui.TableSetColumnIndex(0);
+                    ImGui.TextUnformatted("/pld");
+                    ImGui.TableSetColumnIndex(1);
+                    ImGui.TextUnformatted("Paladin");
+
+                    ImGui.TableNextRow();
+                    ImGui.TableSetColumnIndex(0);
+                    ImGui.TextUnformatted("/whm");
+                    ImGui.TableSetColumnIndex(1);
+                    ImGui.TextUnformatted("White Mage");
+
+                    ImGui.TableNextRow();
+                    ImGui.TableSetColumnIndex(0);
+                    ImGui.TextUnformatted("/drk");
+                    ImGui.TableSetColumnIndex(1);
+                    ImGui.TextUnformatted("Dark Knight");
+
+                    ImGui.TableNextRow();
+                    ImGui.TableSetColumnIndex(0);
+                    ImGui.TextUnformatted("/fsh");
+                    ImGui.TableSetColumnIndex(1);
+                    ImGui.TextUnformatted("Fisher");
+
+                    ImGui.TableNextRow();
+                    ImGui.TableSetColumnIndex(0);
+                    ImGui.TextUnformatted("/pgl");
+                    ImGui.TableSetColumnIndex(1);
+                    ImGui.TextUnformatted("Pugilist");
+
+                    ImGui.EndTable();
+                }
+                ImGui.Spacing();
+                ImGui.TextUnformatted("The plugin equips your best saved gearset for that class/job.");
+                ImGui.EndTooltip();
+            }
+            ImGui.PopStyleVar(2);
+            ImGui.PopID();
+            if (classJobEnabled != configuration.RegisterClassJobs)
             {
                 configuration.RegisterClassJobs = classJobEnabled;
                 configuration.Save();
             }
+            ImGui.EndGroup();
 
             ImGui.BeginGroup();
             var phantomJobsEnabled = configuration.RegisterPhantomJobs;
